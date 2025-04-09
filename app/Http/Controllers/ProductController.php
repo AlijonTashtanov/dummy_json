49e3f5d71f78a71286ce64cec078e173d72160f0
Alijon-Tashtanov <tashtanov743@gmail.com>
2025-04-09 10:50:40 +0500

    changed ProductControllers logic

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -17,7 +17,7 @@ class ProductController extends Controller
         $products = $this->apiService->fetchIphones();
         $this->repository->saveProducts($products);
 
-        return response()->json(['message' => 'iPhone mahsulotlari saqlandi!', 'count' => count($products)]);
+        return response()->json(['message' => 'iPhone products saved!', 'count' => count($products)]);
     }
 
     public function index(): JsonResponse
